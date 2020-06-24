6428b42cc03bba93770600f3daaff120f07ddd42
Thomas Lutz <tlutz65@gmail.com>
2020-06-23 23:10:23 -0700

    renders react

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,6 +26,11 @@
  * ```
  */
 
-import './index.css';
+// import './index.css';
+// import * as React from 'react';
+// import { render } from 'react-dom';
+// import App from './App';
+
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
+
