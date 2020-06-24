1307ffb016b9e432db8eea801648a874597d360e
Louis Xavier Sheid III <louisxsheid@gmail.com>
2020-06-24 11:49:28 -0700

    oauth working/cleaner code

--- src/server/config/passport-setup.ts
+++ src/server/config/passport-setup.ts
@@ -1,15 +1,10 @@
 export {};
 
-const passport = require('passport');
 require('dotenv').config();
-
+const passport = require('passport');
 const GithubStrategy = require('passport-github2');
-
 const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
 
-// const db = require('../db/postgres.js');
-
-const fetch = require('node-fetch');
 console.log("INSIDE PASSPORTSETUP" , GITHUB_CLIENT_ID)
 // Configure the Github strategy for use by Passport.
 //
@@ -30,7 +25,7 @@ passport.use(
       //callback url that sends client to github login page
       callbackURL: '/auth/github/callback',
     },
-    (accessToken: string, refreshToken: string, profile: object | null, done: ()=> void) => {
+    (accessToken: string, refreshToken: string, profile: any, done: Function) => {
       //basic 4 params -> getting github profile information from auth-route
       /** passport callback fn
        * accessToken - is how we will make an API call on behalf of the user. It is sent to us by github in the response.
@@ -39,25 +34,13 @@ passport.use(
        * done - after getting successully authenticated - run this callback function
        * routes to 'authenticated page' w/ correct user information
        **/
-      console.log("INSIDE PASSPORTSETUP FUNCTION")
       const { username } = profile;
       console.log('PASSPORT CALLBACK FIRED FOR USER: ', username);
-
-      // const selectQuery = `SELECT * FROM users WHERE githandle='${username}'`;
-      // const insertQuery = `INSERT INTO users (id, githandle) VALUES (uuid_generate_v4(), $1) RETURNING *`;
-      // db.query(selectQuery)
-      //   .then(data => {
-      //     if (data.rows.length > 0) {
-      done();
-      //     } else {
-      //       db.query(insertQuery, [username])
-      //         .then(user => {
-      //           return done(null, accessToken);
-      //         })
-      //         .catch(err => console.log('INSERT QUERY', err));
-      //     }
-      //   })
-      //   .catch(err => console.log('SELECT QUERY', err));
+      let payload: object = {
+        username,
+        accessToken
+      };
+      done(null, payload);
     }
   )
 );
@@ -69,16 +52,12 @@ passport.use(
  * supply the user ID when serializing, and query the user record by ID
  * from the database when deserializing.
  **/
-// passport.serializeUser(function (user, done) {
-//   console.log('IN SERIALIZE ', user)
-//   done(null, user);
-// });
+passport.serializeUser(function (payload: object, done: Function) {
+  done(null, payload);
+});
 
-// passport.deserializeUser(function (obj, done) {
-//   const findUserQuery = `SELECT * FROM users WHERE id = $1`;
-//   db.query(findUserQuery, [id]).then(user => {
-//     done(null, user); // done is used to progress to the next middleware
-//   });
-// });
+passport.deserializeUser(function (obj: object, done: Function) {
+    done(null);
+});
 
-// module.exports = passport.deserializeUser;
+module.exports = passport.serializeUser, passport.deserializeUser ;
