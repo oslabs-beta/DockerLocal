c4e46c72148960a8109d9c81d5e8457997d25360
Thomas Lutz <tlutz65@gmail.com>
2020-06-25 12:20:49 -0700

    create AddRepos component

---
+++ src/client/components/addRepos/AddRepos.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+
+type Repo = {
+  name: string;
+  link: string;
+}
+
+const AddRepos: React.FC = ({showAddRepos, setShowAddRepos, userInfo}) => {
+  const [repos, setRepos] = useState<Repo[]>([])
+  
+  
+  // dummy request and response 
+  const fetchRepos = (userInfo) => {
+    // send request to back end
+    const response: [] = [
+      {name: 'repo1', link: 'link1'},
+      {name: 'repo2', link: 'link2'},
+      {name: 'repo3', link: 'link3'},
+      {name: 'repo4', link: 'link4'},
+      {name: 'repo5', link: 'link5'},
+      {name: 'repo6', link: 'link6'},
+    ]
+
+    return response;
+  }
+
+  const handleSubmit = () => {
+    // submit stuff to back end
+    // then
+    setShowAddRepos(false);
+
+  }
+
+  useEffect(() => {
+    // fetchRepos(userInfo)
+    // then
+    setRepos(fetchRepos(userInfo));
+  }, [])
+
+  return (
+    <div>
+      adding repos
+      <ul>
+        {/* change this to component with checkmark etc, look for search box style */}
+        {repos.map(repo => <li>{repo.name}</li>)}
+      </ul>
+      <button onClick={handleSubmit}>Close</button>
+    </div>
+  )
+}
+
+export default AddRepos;
--- src/client/components/projects/Projects.tsx
+++ src/client/components/projects/Projects.tsx
@@ -1,10 +1,10 @@
 import React, { useState, Dispatch, SetStateAction, useEffect } from 'react';
 
-import Sidebar from '../sidebar/Sidebar'
+import Sidebar from '../sidebar/Sidebar';
+import AddRepos from '../addRepos/AddRepos'
 
 type ProjectsProps = {
   userInfo: {username: string; id: string};
-  // need to do more research on Dispatch, SetStateAction and ts: https://stackoverflow.com/questions/56028635/passing-usestate-as-props-in-typescript
   setUserInfo: Dispatch<SetStateAction<{username: string; id: string}>>;
 }
 
@@ -13,7 +13,7 @@ const Projects: React.FC<ProjectsProps> = ({ userInfo, setUserInfo }) => {
   // need to set type for projects/projectlist
   const [projectList, setProjectList] = useState([{projectName: 'DockerLocal(project1)', projectId: 1}, {projectName: 'React Visualizer 74.0(project2)', projectId: 2}]);
   const [activeProject, setActiveProject] = useState({projectName: 'DockerLocal(project1)', projectId: 1});
-  const [toggleAddRepos, setToggleAddReops] = useState(false);
+  const [showAddRepos, setShowAddRepos] = useState(false);
 
 
   return (
@@ -22,8 +22,13 @@ const Projects: React.FC<ProjectsProps> = ({ userInfo, setUserInfo }) => {
       {/* <LoggedIn/> << logged in component at top with logout button and username*/}
       {`${userInfo.username}`}
       <Sidebar {...{projectList, activeProject, setActiveProject}} />
-    <button onClick={(): void => setToggleAddReops(true)}>Add Reops to {activeProject.projectName}</button>
-    
+    <button onClick={(): void => setShowAddRepos(true)}>Add Repos to {activeProject.projectName}</button>
+
+    {/* shows this element if showAddRepos is true */}
+    {showAddRepos && <AddRepos
+                        {...{showAddRepos, setShowAddRepos, userInfo}}
+                      />}
+
     </div>
   )
 }
