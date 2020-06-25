c226bc92dcd0a2dcb407e774f982a5896fb48302
KateChantha <kubkate@gmail.com>
2020-06-25 11:42:30 -0700

    SignIn button and AppInfo button- display and works

--- src/client/components/signIn/SignIn.tsx
+++ src/client/components/signIn/SignIn.tsx
@@ -3,21 +3,19 @@ import React, { useState } from "react";
 import AppInfoModal from "../appInfo/AppInfoModal";
 
 const SignIn: React.FC = () => {
+  //initailize showModal to false
   const [showModal, setShowModal] = useState(false);
 
+  //display signIn button and appInfo button
   return (
     <div>
-      <div className="imgWrap">
-        <img
-          src="https://www.freeiconspng.com/uploads/github-logo-icon-24.png"
-          alt="Png Vector Github Logo"
-          onClick={() =>
-            window.location.replace("http://localhost:3001/auth/github")
-          }
-          className="githublogo"
-          width="100"
-        />
-      </div>
+      <button
+        onClick={() =>
+          window.location.replace("http://localhost:3001/auth/github")
+        }
+      >
+        Sign In With Github
+      </button>
       <button onClick={() => setShowModal(!showModal)}>
         More Info About DockerLocal
       </button>
