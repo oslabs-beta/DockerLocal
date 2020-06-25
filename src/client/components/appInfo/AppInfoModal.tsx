cee73fe4ad3b95552707ca95026cc2beadf99539
KateChantha <kubkate@gmail.com>
2020-06-25 11:13:19 -0700

    Add AppInfoModal component

--- src/client/components/appInfo/AppInfoModal.tsx
+++ src/client/components/appInfo/AppInfoModal.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import ReactDom from "react-dom";
 
-const modalAppInfo = document.getElementById("modal-appInfo");
+// const modalAppInfo = document.getElementById("modal-appInfo");
 
 const AppInfo: React.FC = () => {
-  return ReactDom.createPortal(<div>This is appInfo modal</div>, modalAppInfo);
+  // return ReactDom.createPortal(<div>This is appInfo modal</div>, modalAppInfo);
+  return <div>This is appInfo modal</div>;
 };
 
 export default AppInfo;
