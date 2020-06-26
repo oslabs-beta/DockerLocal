b8d083d6705b464bff840f99392949f30f9c6fb3
Thomas Lutz <tlutz65@gmail.com>
2020-06-25 17:39:28 -0700

    add bulma style library

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -2,8 +2,8 @@ const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
 
 rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  test: /\.s[ac]ss$/i,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
 });
 
 module.exports = {
