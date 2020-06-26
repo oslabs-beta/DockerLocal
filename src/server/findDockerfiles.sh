3548bb49ad6d19951e2bb650901c5cbd99f6591c
Vivian Cermeno <viviancermeno@Vivians-MacBook-Pro.local>
2020-06-26 16:40:23 -0700

    Retrieve build file paths for yaml template

---
+++ src/server/findDockerfiles.sh
@@ -0,0 +1,7 @@
+# Find Dockerfiles in chosen repository
+# check if dockerfile exists here
+# it will find the docker file in the directory in which exec_shell.js is called
+
+FILE=*[dD]ocker*
+# Folder path WILL change in production
+find . -name "$FILE"
