8976198036c2611836fe9b3dcb6780606fb1facc
KateChantha <kubkate@gmail.com>
2020-06-26 17:22:46 -0700

    Setup folder and file for  GetStarted and AppInfoList components

---
+++ src/client/components/getStarted/GetStarted.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import Home from "../home/home";
+// import Sidebar from "../sidebar/Sidebar";
+// import AddRepos from "../addRepos/AddRepos";
+
+// place holder for GetStarted components
+//TODO: add logic to render child components
+const GetStarted = () => {
+  return <div>Get Started Page</div>;
+};
+
+export default GetStarted;
