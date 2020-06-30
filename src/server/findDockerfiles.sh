40d28ea039507ad06ff5aad6c79469a1fe335e08
Vivian Cermeno <viviancermeno@Vivians-MacBook-Pro.local>
2020-06-30 09:54:24 -0700

    delete files made accidentally in master branch

--- src/server/findDockerfiles.sh
+++
@@ -1,7 +0,0 @@
-# Find Dockerfiles in chosen repository
-# check if dockerfile exists here
-# it will find the docker file in the directory in which exec_shell.js is called
-
-FILE=*[dD]ocker*
-# Folder path WILL change in production
-find . -name "$FILE"
