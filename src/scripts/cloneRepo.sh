68e5ffbfa0c991dd74eb4896a2b21a22fe7d21dc
Katty <kpolyak1@binghamton.edu>
2020-06-30 14:48:21 -0700

    add additional comments to ssh key and clone repo scripts

--- src/scripts/cloneRepo.sh
+++ src/scripts/cloneRepo.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # Script to clone a github repository
 
+# handle incoming arguments, username and repoName
 username=$1
 repoName=$2
 
-echo $1 $2
-
+# get the sock address of our ssh agent so that we can connect to it
 temp_sock=$(cat ./tmpAgent/agentSock)
+# add the ssh agent sock to the environment variables
 export SSH_AUTH_SOCK=$temp_sock
 
+# create a myRepos folder to store repositories
 mkdir ./myRepos
 cd myRepos
 
+# clone a git repository from github using ssh connection
 git clone git@github.com:$username/$repoName.git
 echo "We finished cloning"
 exit 0
