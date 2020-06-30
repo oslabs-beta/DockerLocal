6608e750a232fff9fdf35e38e1ae8aa014bebd2c
Vivian Cermeno <viviancermeno@Vivians-MacBook-Pro.local>
2020-06-30 11:54:19 -0700

    fixed one comment

--- src/scripts/findDockerfiles.sh
+++ src/scripts/findDockerfiles.sh
@@ -1,5 +1,4 @@
 # Find Dockerfiles in chosen repository
-# it will find the docker file in the directory in which exec_shell.js is called
 
 FILE=*[dD]ocker*
 # Folder path WILL change in production
