276fbfa59a03765b8f8539e4d88d2c5313329535
Vivian Cermeno <viviancermeno@Vivians-MacBook-Pro.local>
2020-07-01 16:14:52 -0700

    reformat project folder in shell script

--- src/scripts/findDockerfiles.sh
+++ src/scripts/findDockerfiles.sh
@@ -1,5 +1,5 @@
 # Find Dockerfiles in chosen repository
-project_folder = $1
+projectFolder = $1
 FILE=*[dD]ocker*
 # Folder path WILL change in production
-find ./myRepos/project_folder -name "$FILE" -type f
+find ./myRepos/projectFolder -name "$FILE" -type f
