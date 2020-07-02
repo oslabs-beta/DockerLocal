f857f3a34ecfe43010f14c52bcbd5a00e7eff93b
Vivian Cermeno <viviancermeno@Vivians-MacBook-Pro.local>
2020-07-02 15:25:21 -0700

    Alters Toggle isLoggedIn button for production

--- src/client/components/home/Routes.tsx
+++ src/client/components/home/Routes.tsx
@@ -30,7 +30,7 @@ const Routes: React.FC = (props) => {
   
   return (
     <div>
-      <button onClick={(): void=> setIsLoggedIn(!isLoggedIn)} >TOGGLE isLoggedIn FOR DEV</button>
+      <button onClick={(): void=> setIsLoggedIn(!isLoggedIn)} >isLoggedIn</button>
       {rendered}
     </div>
   );
