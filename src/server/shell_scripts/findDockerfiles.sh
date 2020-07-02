d3fc85a3e387a4f2f6c83a137d52956ccd791b7b
Vivian Cermeno <viviancermeno@Vivians-MacBook-Pro.local>
2020-07-01 23:26:13 -0700

    delete files not included in master

--- src/server/shell_scripts/findDockerfiles.sh
+++
@@ -1,6 +0,0 @@
-# Find Dockerfiles in chosen repository
-# it will find the docker file in the directory in which exec_shell.js is called
-
-FILE=*[dD]ocker*
-# Folder path WILL change in production
-find ./myRepos -name "$FILE" -type f
