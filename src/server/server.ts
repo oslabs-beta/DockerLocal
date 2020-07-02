e3d2bf1464065f9733f2bf41ce7c45d46653e6fa
Louis Xavier Sheid III <louisxsheid@gmail.com>
2020-07-01 20:13:21 -0700

    add read/write from file for saved projects

--- src/server/server.ts
+++ src/server/server.ts
@@ -12,6 +12,7 @@ const cookieParser = require("cookie-parser");
 // Bring in routes
 const authRoute = require("../../src/server/routes/auth-route");
 const apiRoute = require("../../src/server/routes/api-route");
+const saveRoute = require("../../src/server/routes/save-route");
 
 // Body Parsing Middleware
 app.use(express.json());
@@ -22,6 +23,7 @@ app.use(cookieParser());
 // Use routes
 app.use("/auth", authRoute);
 app.use("/api", apiRoute);
+app.use("/save", saveRoute);
 
 // Serve static files
 app.use(express.static("assets"));
