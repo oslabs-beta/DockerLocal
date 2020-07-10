59e1f4ee7680d093ffe399f45c4953a13c7391dd
Katty <kpolyak1@binghamton.edu>
2020-07-09 21:12:01 -0700

    finish valid project name functionality

--- src/client/components/projects/ProjectSidebar.tsx
+++ src/client/components/projects/ProjectSidebar.tsx
@@ -1,72 +1,120 @@
-import React, { Dispatch, SetStateAction, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { Project, ProjectSideBarProps } from "../../../types/types";
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Project, ProjectSideBarProps } from '../../../types/types';
+
+import { checkValidName } from '../../helpers/projectHelper';
 
 const ProjectSideBar: React.FC<ProjectSideBarProps> = ({
   setShowProjectSidebarModal,
   projectList,
   setProjectList,
 }) => {
-  const [projectNameValue, setProjectNameValue] = useState("");
+  // State hook for new project name and boolean whether the name is valid
+  const [newProject, setNewProject] = useState({
+    name: '',
+    isValid: false,
+  });
 
+  // State hook for message body and style
+  const validBody =
+    'Project names can contain alphanumeric characters, hypens, or underscores. Project names must be unique, contain less than 26 characters, and cannot be blank.';
+  const invalidBody = `You have entered an invalid project name. ${validBody}`;
+  const [newProjectStyle, setNewProjectStyle] = useState({
+    messageBody: validBody,
+    messageStyle: 'is-info',
+  });
+
+  /**
+   * @function handleChange
+   * @description updates newProject.name, newProject.isValid, newProjectStyle.messageBody, newProjectStyle.messageStyle
+   * @param e each update to new project name field
+   */
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setProjectNameValue(e.target.value);
+    const isValid = checkValidName(e.target.value, projectList);
+    setNewProject({
+      name: e.target.value,
+      isValid: isValid,
+    });
+    setNewProjectStyle({
+      messageBody: isValid ? validBody : invalidBody,
+      messageStyle: isValid ? 'is-info' : 'is-danger',
+    });
+    console.log(newProject);
   };
 
-  // ============= TODO: ==================
-  // 1. improve logic to handle project name input edge cases
-  //     1.1 handle empty string
-  //     1.2 handle max lenght
-  // 2. on setProjectLise, add logic to write to file
-  // 3. add more comments
-  // ========================================
-  const handleSubmit = () => {
-    //create an object
-    const newProject: Project = {
-      projectId: uuidv4(),
-      projectName: projectNameValue,
-      projectRepos: [],
-    };
+  const handleSubmit = (): void => {
+    //create a new project object
+    if (checkValidName(newProject.name, projectList)) {
+      const newProjectObject: Project = {
+        projectId: uuidv4(),
+        projectName: newProject.name,
+        projectRepos: [],
+      };
 
-    // set copy of a new project
-    setProjectList([...projectList, newProject]);
-    //TODO:  write to the file ******
-    //then close the modal
-    setShowProjectSidebarModal(false);
+      // set copy of a new project
+      setProjectList([...projectList, newProjectObject]);
+      //TODO:  write to the file ******
+      //then close the modal
+      setShowProjectSidebarModal(false);
+    }
   };
+
+  const errorIcon = (
+    <span className='icon has-text-danger is-right'>
+      <i className='fas fa-exclamation'></i>
+    </span>
+  );
 
   // input for projectname
   return (
-    <div className="modal is-active">
-      <div className="modal-background"></div>
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <p className="modal-card-title">Create A New Project</p>
-          <button className="delete" aria-label="close"></button>
+    <div className='modal is-active'>
+      <div className='modal-background'></div>
+      <div className='modal-card'>
+        <header className='modal-card-head'>
+          <p className='modal-card-title'>Create A New Project</p>
+          <button className='delete' aria-label='close'></button>
         </header>
-        <section className="modal-card-body">
+        <section className='modal-card-body'>
           <form onSubmit={handleSubmit}>
             <label>
-              Project Name:
-              <input
-                type="text"
-                placeholder="Project Name"
-                value={projectNameValue}
-                size={70}
-                onChange={handleChange}
-              />
+              <div className={'message ' + newProjectStyle.messageStyle}>
+                <div className='message-header'>
+                  <p>Please choose a name for your project.</p>
+                </div>
+                <div className='message-body'>
+                  {newProjectStyle.messageBody}
+                </div>
+              </div>
+              <span className='control has-icons-right'>
+                <input
+                  autoFocus
+                  type='text'
+                  className={'input ' + newProjectStyle.messageStyle}
+                  placeholder='Project Name'
+                  value={newProject.name}
+                  size={70}
+                  onChange={handleChange}
+                />
+                {!newProject.isValid && newProject.name !== '' && errorIcon}
+                {!newProject.isValid && newProject.name !== '' && (
+                  <p className='has-text-danger has-text-weight-bold'>
+                    Invalid project name
+                  </p>
+                )}
+              </span>
             </label>
           </form>
         </section>
-        <footer className="modal-card-foot">
+        <footer className='modal-card-foot'>
           <button
-            className="button is-success"
+            className='button is-success'
             onClick={(): void => handleSubmit()}
+            disabled={!newProject.isValid}
           >
             Create Project
           </button>
           <button
-            className="button"
+            className='button'
             onClick={(): void => setShowProjectSidebarModal(false)}
           >
             Cancel
--- src/client/helpers/projectHelper.ts
+++ src/client/helpers/projectHelper.ts
@@ -1,5 +1,11 @@
-import { Project } from "../../types/types";
+import { Project } from '../../types/types';
 
+/**
+ * @function findActiveProject
+ * @description finds the active project
+ * @param projectList
+ * @param activeProject
+ */
 export const findActiveProject = (
   projectList: readonly Project[],
   activeProject: string
@@ -7,4 +13,24 @@ export const findActiveProject = (
   return {
     ...projectList.find((project) => project.projectId === activeProject),
   };
+};
+/**
+ * @function checkValidName
+ * @description function that updates boolean in state that indicates whether project name entered is valid
+ * @param projectNameValue
+ * @returns boolean. True if no invalid characters
+ */
+export const checkValidName = (
+  projectNameValue: string,
+  projectList: readonly Project[]
+): boolean => {
+  // RegExp to test if project name will be valid project/folder name.
+  // Can have letters, numbers, hyphens, underscores only. Limited to 1 to 25 characters
+  const regex = /^[\w-]{1,25}$/;
+  const isValid: boolean = regex.test(projectNameValue);
+  // Check to make sure we don't have duplicate project names.
+  const nameArray = projectList.map((project) => project.projectName);
+  const isUnique = !nameArray.includes(projectNameValue);
+
+  return isValid && isUnique;
 };
