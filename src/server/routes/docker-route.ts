36e7b760f3a7d37f1c37ac2867a158e2852b8870
Louis Xavier Sheid III <louisxsheid@gmail.com>
2020-07-09 17:51:18 -0700

    leave one out functionality

--- src/server/routes/docker-route.ts
+++ src/server/routes/docker-route.ts
@@ -2,11 +2,13 @@ export { };
 import express, { Request, Response } from 'express';
 import path from 'path';
 const router = express.Router();
+const authController = require('../controllers/authController');
 const dockerController = require('../controllers/dockerController');
 
 // creates docker compose file using project folder names
 router.post(
     '/',
+    authController.saveUserInfoAndRepos,
     dockerController.getFilePaths,
     dockerController.getContainerNames,
     dockerController.createDockerCompose,
@@ -14,7 +16,9 @@ router.post(
          // sends compose file to front end
          const projectFolder = req.body.projectName;
          // sends compose file to front end
-         res.status(200).sendFile(path.resolve(__dirname, `../../myRepos/${projectFolder}/docker-compose.yaml`));
+
+         //SEND BACK PATH 
+         res.status(200).sendFile(path.resolve(__dirname, `../../myProjects/${projectFolder}/docker-compose.yaml`));
      }
     )
 
