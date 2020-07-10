88818b2b553ef16a88f8a63e7b90ee0d56c948ad
Vivian Cermeno <viviancermeno@Vivians-MacBook-Pro.local>
2020-07-10 16:44:04 -0700

    fix login button issue and display username on login

--- src/client/helpers/cookieClientHelper.ts
+++ src/client/helpers/cookieClientHelper.ts
@@ -4,17 +4,28 @@ export const getUsernameAndToken = async (): Promise<{
   username: string;
   accessToken: string;
 }> => {
-  //PARSE TOKEN AND USERNAME FROM COOKIES
+  // PARSE TOKEN AND USERNAME FROM COOKIES
+  let username;
+  let token;
   const nameAndToken = document.cookie.split(";");
-  const username = nameAndToken[0].replace("username=", "").trim();
-  const token = nameAndToken[1].replace("token=", "").trim();
+  /* the cookie comes in with either token or username first since we dont know when or why that happens
+   this function is supposed to check which one comes first and assign it respectively before parsing */
+  if (nameAndToken[0].slice(0,9) === "username"){
+    username = nameAndToken[0];
+    token = nameAndToken[1];
+  } else {
+    username = nameAndToken[1];
+    token = nameAndToken[0];
+  }
+  username = username.replace("username=", "").trim();
+  token = token.replace("token=", "").trim();
   const parsedToken = decodeURIComponent(token);
 
-  //DECRYPT TOKEN FROM COOKIES
+  // DECRYPT TOKEN FROM COOKIES
   const decryptedToken = await CryptoJS.AES.decrypt(
     parsedToken,
     "super_secret"
   ).toString(CryptoJS.enc.Utf8);
 
-  return { username: username, accessToken: decryptedToken };
+  return { username, accessToken: decryptedToken };
 };
