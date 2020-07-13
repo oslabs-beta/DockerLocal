2bd3d31410c6dd64960dc8e3e1492dba4670f1ae
Thomas Lutz <tlutz65@gmail.com>
2020-07-12 21:11:45 -0700

    pass electron security tutorial

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { app, BrowserWindow, Menu, session } from 'electron';
+import { app, BrowserWindow, shell, session, WebPreferences } from 'electron';
 import installExtension, {
   REACT_DEVELOPER_TOOLS,
 } from 'electron-devtools-installer';
+import { EXPRESS_URL } from "./client/helpers/constants";
+
 
 
 const express = require('./server/server.ts');
@@ -18,7 +20,7 @@ declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 // const viewSettingsMenu = new Menu()
 
 
-let mainWindow: BrowserWindow;
+// let mainWindow: BrowserWindow;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 // eslint-disable-line global-require
@@ -26,16 +28,16 @@ if (require('electron-squirrel-startup')) app.quit();
 
 const createWindow = (): void => {
   // // Create the browser window.
-  mainWindow = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
     autoHideMenuBar: true,
     useContentSize: true,
     resizable: true,
     webPreferences: {
-      nodeIntegration: false,
       contextIsolation: true,
-      webSecurity: true,
+      enableRemoteModule: false
+
     }
     
   
@@ -91,6 +93,45 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+
+// Verify WebView Options Before Creation - from electron security docs - https://www.electronjs.org/docs/tutorial/security#11-verify-webview-options-before-creation
+app.on('web-contents-created', (event, contents) => {
+  contents.on('will-attach-webview', (event, webPreferences: WebPreferences & { preloadURL: string}, params) => {
+    // Strip away preload scripts if unused or verify their location is legitimate
+    delete webPreferences.preload;
+    delete webPreferences.preloadURL;
+
+    // Disable Node.js integration
+    webPreferences.nodeIntegration = false;
+
+    // No URLS should be loaded
+    event.preventDefault();
+    
+  })
+})
+
+const URL = require('url').URL
+
+// only allows navigation to our express server URL
+app.on('web-contents-created', (event, contents) => {
+  contents.on('will-navigate', (event, navigationUrl) => {
+    const parsedUrl = new URL(navigationUrl)
+    if (parsedUrl.origin !== EXPRESS_URL) {
+      event.preventDefault()
+    }
+  })
+})
+
+// doesnt allow new windows to be created
+app.on('web-contents-created', (event, contents) => {
+  contents.on('new-window', async (event, navigationUrl) => {
+    event.preventDefault()
+    await shell.openExternal(navigationUrl)
+  })
+})
+
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
