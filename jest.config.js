4b2d375d476f1f6725c46291b9757918f11e2333
KateChantha <kubkate@gmail.com>
2020-07-13 15:09:12 -0700

    Initial setup

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testEnvironment: 'node',
+  // compile TSX to JavaScript using ts-jest preset
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  // Matches patrent folder `__test__` and file name shold contain `test`
+  testRegex: '/__tests__/.*\\.test.(ts|tsx)$',
+  // Module file extension for importing
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // Path to setupTest.ts file for testin environment to be run immediately before running the test code.
+  setupFiles: ['<rootDir>/__tests__/setupTests.ts']
+};
