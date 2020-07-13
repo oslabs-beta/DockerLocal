0f7899306e7f4049beeb174467f3dcfe7fa6b8c4
Vivian Cermeno <viviancermeno@Vivians-MacBook-Pro.local>
2020-07-13 14:16:05 -0700

    remove unused code

--- src/server/routes/api-route.ts
+++ src/server/routes/api-route.ts
@@ -6,19 +6,6 @@ const sshKeyController = require("../controllers/sshKeyController");
 const router = require("express").Router();
 require("dotenv/config");
 
-// -> /api/repos will get the username and access token from cookies then fetch a list
-//  of user repos (and display these repos as json on the client side)
-// router.get(
-//   "/repos",
-//   authController.getNameAndTokenFromCookies,
-//   apiController.getUserRepos,
-//   sshKeyController.createSSHkey,
-//   sshKeyController.addSSHkeyToGithub,
-//   gitController.cloneRepo,
-//   sshKeyController.deleteSSHkey,
-//   (req: Request, res: Response) => res.status(200).json(res.locals.repos)
-// );
-
 router.post(
   "/clonerepos",
   authController.saveUserInfoAndRepos,
