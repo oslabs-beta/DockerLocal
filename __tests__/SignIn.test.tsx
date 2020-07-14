309c1e4c8623209d3f23dcc6c814858019a45a45
KateChantha <kubkate@gmail.com>
2020-07-13 22:23:03 -0700

    Rename to tsx

--- __tests__/SignIn.test.tsx
+++ __tests__/SignIn.test.tsx
@@ -7,6 +7,10 @@ import AppInfoModal from '../src/client/components/appInfo/AppInfoModal'
 describe('<SignIn/>', () => {
   it('should render <AppInfoModal /> element if showModal evaluates to true', () => {
     const wrapper = shallow(<SignIn />);
+    console.log('test');
+    console.log(wrapper.debug());
     expect(wrapper.find(AppInfoModal)).toHaveLength(2);
+    // expect(1).toBe(1);
   })
 })
+
--- __tests__/setupTest.ts
+++ __tests__/setupTest.ts
@@ -1,4 +1,5 @@
-import enzyme from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-enzyme.configure({ adapter: new Adapter() });
+// setup the adapter to be use by enzyme
+configure({ adapter: new Adapter() });
