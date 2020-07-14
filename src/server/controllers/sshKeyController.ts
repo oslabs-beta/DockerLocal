495ca921aa358f9fb2793387ed349050fd5730f9
Vivian Cermeno <viviancermeno@Vivians-MacBook-Pro.local>
2020-07-14 15:13:00 -0700

    Add error handling and return types to sshKeyController

--- src/server/controllers/sshKeyController.ts
+++ src/server/controllers/sshKeyController.ts
@@ -13,20 +13,23 @@ const sshKeyController: any = {};
  * @middleware  Create SSH key to be used for connection to clone/update github repos
  * @desc    Executes sshKeygen.sh
  */
-sshKeyController.createSSHkey = async (
+sshKeyController.createSSHkey = async(
   req: Request,
   res: Response,
   next: NextFunction
-) => {
-  
+): Promise<void> => {
+
   // shell script adds the github.com domain name to the known_hosts file using the ssh-keyscan command
   // script then clones github repo using SSH connection
   const shellCommand = "./src/scripts/sshKeygen.sh";
-  
-  const shellResult = await execShellCommand(shellCommand, []);
-  console.log('request')
-  console.log(shellResult);
-  console.log("Finished Generating a Key");
+
+  const shellResult = await execShellCommand(shellCommand, [])
+  if (shellResult instanceof Error){
+    return next({
+      log: `Error caught in sshKeyController.createSSHkey: execShellCommand produces error: ${shellResult}`,
+      msg: {err:`sshKeyController.createSSHkey: ERROR: Check server logs for details`},
+    })
+  }
   return next();
 };
 
@@ -38,10 +41,9 @@ sshKeyController.addSSHkeyToGithub = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): Promise<void> => {
   const { accessToken, username } = res.locals;
 
-  console.log("before read sshKey");
   const sshKey = fs.readFileSync("./tmpKeys/dockerKey.pub", "utf8");
 
   // create the request body which we will use to create the ssh key on Github
@@ -59,7 +61,10 @@ sshKeyController.addSSHkeyToGithub = async (
       "Content-type": "application/json",
     },
     body: reqBody,
-  });
+  }).catch((err: Error) => next({
+    log:  `Error caught in sshKeyController.addSSHkeyToGithub: Issue sending post request to Github API: ${err}`,
+    msg: {err:'sshKeyController.addSSHkeyToGithub: ERROR: Check server logs for details'}
+  }));
 
   // converts the response body into JSON
   const jsonResponse = await response.json();
@@ -67,7 +72,7 @@ sshKeyController.addSSHkeyToGithub = async (
   // save the key id from the response. this will be used to delete the key from Github after we are done using it
   const { id } = jsonResponse;
   res.locals.keyId = id;
-  console.log("Finished adding key to github");
+
   return next();
 };
 
@@ -79,7 +84,7 @@ sshKeyController.deleteSSHkey = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): Promise<void> => {
   const { accessToken, keyId } = res.locals;
 
   const shellCommand = "./src/scripts/sshKeyDelete.sh";
@@ -87,13 +92,16 @@ sshKeyController.deleteSSHkey = async (
   await execShellCommand(shellCommand, []);
 
   const url = `https://api.github.com/user/keys/${keyId}`;
-  const response = await fetch(url, {
+  await fetch(url, {
     method: "delete",
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
-  });
-  console.log("Finished Deleting Key");
+  }).catch((err: Error) => next({
+    log:  `Error caught in sshKeyController.deleteSSHkey: deleting key from github produces error: ${err}`,
+    msg: {err: 'sshKeyController.deleteSSHkey: ERROR: Check server logs for details'}
+  }));
+
   return next();
 };
 
